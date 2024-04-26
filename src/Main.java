fe876dcb356c7c196c996bd97f3ac2a6198062c7
Azat <vildus@yandex.ru>
2024-04-27 02:11:04 +0300

    HW от 27.04.2024

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,29 @@ public class Main {
         // Первое задание
 
         System.out.println("\nTask 1 \n");
-        byte clientOS = 3;      // Объявляем переменную clientOS  1 это iOS; 2 это android
-        if (clientOS == 1) {    // Оператор if выполняется в случае если у оператора iOS
+        byte clientOS = 1;      // Объявляем переменную clientOS  0 это iOS; 1 это android
+        if (clientOS == 0) {    // Оператор if выполняется в случае если у оператора iOS
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 2) {   // Оператор else if выполняется в случае если у оператора Android
+        } else if (clientOS == 1) {   // Оператор else if выполняется в случае если у оператора Android
             System.out.println(("Установите версию приложения для Android по ссылке"));
         }
-        else {    // Оператор else выполняется в случае если введено другая цифра в отличии от 1 или 2
+        else {    // Оператор else выполняется в случае если введено другая цифра в отличии от 0 или 1
             System.out.println("Не корректный ввод!");
         }
 
         // Второе задание
 
         System.out.println("\nTask 2 \n");
-        int clientOSTwo = 1;      // Объявляем переменную clientOSTwo  1 это iOS; 2 это android
-        int clientDeviceYear = 2016;  // Объявляем переменную clientDeviceYear (год выпуска телефона)
-        if (clientDeviceYear > 2015 && clientOSTwo == 1) {  // Оператор if выполняется в случае если у оператора iOS и телефон выпущен после 2015 года
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear > 2015 && clientOSTwo == 2) { // Оператор else if выполняется в случае если у оператора android и телефон выпущен после 2015 года
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOSTwo == 1) {  // Оператор else if выполняется в случае если у оператора iOS и телефон выпущен d 2015 году или ранее
+        int clientOSTwo = 1;      // Объявляем переменную clientOSTwo  0 это iOS; 1 это android
+        int clientDeviceYear = 2015;  // Объявляем переменную clientDeviceYear (год выпуска телефона)
+        if (clientDeviceYear >= 2015 && clientOSTwo == 0) {  // Оператор if выполняется в случае если у оператора iOS и телефон выпущен после 2015 года
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOSTwo == 2) {  // Оператор else if выполняется в случае если у оператора android и телефон выпущен d 2015 году или ранее
+        } else if (clientDeviceYear >= 2015 && clientOSTwo == 1) { // Оператор else if выполняется в случае если у оператора android и телефон выпущен после 2015 года
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOSTwo == 0) {  // Оператор else if выполняется в случае если у оператора iOS и телефон выпущен d 2015 году или ранее
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear < 2015 && clientOSTwo == 1) {  // Оператор else if выполняется в случае если у оператора android и телефон выпущен d 2015 году или ранее
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else {  // Оператор else выполняется в случае если введено другая цифра в отличии от 1 или 2
             System.out.println("Не корректный ввод!");
         }
